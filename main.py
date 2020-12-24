962d6f8e3627a6629708007e91c01d243d22b405
DⱥℝkͥAnͣgͫeℓ <60337323+Jijinr@users.noreply.github.com>
2020-12-24 22:28:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,12 +30,12 @@ def main():
                 msg = user_client.get_history(bot, 1)[0]
                 if snt.message_id == msg.message_id:
                     print(f"[WARNING] @{bot} is down")
-                    edit_text += f"➲ @{bot} : ❌\n"
+                    edit_text += f"**➲ @{bot} :** ❌\n"
                     user_client.send_message(bot_owner,
                                              f"@{bot} status: `Down`")
                 else:
                     print(f"[INFO] all good with @{bot}")
-                    edit_text += f"➲@{bot} : ✅\n"
+                    edit_text += f"**➲@{bot} :** ✅\n"
                 user_client.read_history(bot)
 
             utc_now = datetime.datetime.utcnow()
